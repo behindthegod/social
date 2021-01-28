a3d6a8d4b5d4a092091334c0c3357b4d4c68f465
Ilya Karlyuk <divizionby0@gmail.com>
2021-01-28 22:53:39 +0300

    up

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+         <header className='header'>
+             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkWV4lQebJxcDTE9dtiBrCBXLivo_AOvbD4w&usqp=CAU'/>
+         </header>
+     )
+ };
+
+ export default Header;
